900eac947e5b0ae089d59711515baf80e8bcf190
KyleFabicon <35807785+KyleFabicon@users.noreply.github.com>
2023-08-24 14:50:45 +0800

    Update disabled.lua

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "goolord/alpha-nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
 }
